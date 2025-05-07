18904d40398eb87a1ab22f3efd8992ad2bdf0e96
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-05-07 20:29:49 +0700

    buat mencapi posisi suatu elemen

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -83,3 +83,19 @@ void binarySearch()
         int high = n - 1;
 
         do
+        {
+            int mid = (low + high) / 2;
+
+            if (elemen[mid] == item)
+            {
+                cout << "\nElemen " << item << " ditemukan pada indeks ke-" << mid << ".\n";
+                return;
+            }
+            else if (item < elemen[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
