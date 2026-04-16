79e2166b98c75f2b44920b45140b504d6a501cc9
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-04-16 15:13:39 +0700

    implement bubbleSortArray

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -32,8 +32,21 @@ void input()
         cout << "data ke-" << i + 1 << ": ";
         cin >> element[i];
     }
+}
 
-void bubbleSortarray() 
-
-
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= npanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1]) {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= npanjang - 1);
 }
